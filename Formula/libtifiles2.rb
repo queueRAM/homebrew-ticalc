a3a157c0d5049f65ab41b0ff797c9ed0f7a4472b
queueRAM <queueRAM@gmail.com>
2021-01-06 23:28:22 -0800

    Add formula for libtifiles2 1.1.7

---
+++ Formula/libtifiles2.rb
@@ -0,0 +1,27 @@
+class Libtifiles2 < Formula
+  desc "TI files library"
+  homepage "http://lpg.ticalc.org/prj_tilp/"
+  url "http://downloads.sourceforge.net/project/tilp/tilp2-linux/tilp2-1.18/libtifiles2-1.1.7.tar.bz2"
+  sha256 "9ac63b49e97b09b30b37bbc84aeb15fa7967bceb944e56141c5cd5a528acc982"
+  license ""
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libarchive"
+  depends_on "libticonv"
+
+  def install
+    system "autoreconf", "-fi"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
