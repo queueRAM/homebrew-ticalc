5b86e7585214a98b4eb64ef1d5460af3a1d804c8
queueRAM <queueRAM@gmail.com>
2021-01-09 23:10:08 -0800

    Add patch for tiemu macOS PCKEY values

--- Formula/tiemu.rb
+++ Formula/tiemu.rb
@@ -585,3 +585,262 @@ diff -aur tiemu-3.03.orig/src/gui/filesel.c tiemu-3.03/src/gui/filesel.c
  #include <string.h>
  
  #ifdef __WIN32__
+diff --git a/src/gui/calc/keynames.c b/src/gui/calc/keynames.c
+index eafea0f..b13a9dc 100755
+--- a/src/gui/calc/keynames.c
++++ b/src/gui/calc/keynames.c
+@@ -136,9 +136,9 @@ const KeyTuple pckeys[] = {
+ 	{ PCKEY_RETURN, "PCKEY_RETURN" },
+ 	{ PCKEY_SHIFT_L, "PCKEY_SHIFT_L" },
+ 	{ PCKEY_CONTROL_L, "PCKEY_CONTROL_L" },
++	{ PCKEY_SHIFT_R, "PCKEY_SHIFT_R" },
+ #ifndef __MACOSX__
+ /* FIXME: We need the key codes for these on OS X. */
+-	{ PCKEY_SHIFT_R, "PCKEY_SHIFT_R" },
+ 	{ PCKEY_CONTROL_R, "PCKEY_CONTROL_R" },
+ #endif
+ 	{ PCKEY_MENU, "PCKEY_MENU" },
+diff --git a/src/gui/calc/pckeys.h b/src/gui/calc/pckeys.h
+index a8a7e3e..72b40bc 100644
+--- a/src/gui/calc/pckeys.h
++++ b/src/gui/calc/pckeys.h
+@@ -339,104 +339,108 @@
+ #define PCKEY_OEM_CLEAR 0xFf
+ 
+ #elif defined(__MACOSX__)
+-// List manually written by Christian Walther
+-#define PCKEY_LBUTTON -2
+-#define PCKEY_RBUTTON -2
+-#define PCKEY_MBUTTON -2
+-#define PCKEY_CANCEL -2
++#define PCKEY_STARTKEY  0x37 // command
++#define PCKEY_MENU      0x3a // option
++#define PCKEY_CONTROL_L 0x3b
++#define PCKEY_SHIFT_L   0x38
++#define PCKEY_SHIFT_R   0x3c
++#define PCKEY_SPACE     0x31
++#define PCKEY_ESCAPE    0x35
++#define PCKEY_TAB       0x30
++#define PCKEY_BACK      0x33
++#define PCKEY_RETURN    0x24
++#define PCKEY_LEFT      0x7b
++#define PCKEY_UP        0x7e
++#define PCKEY_RIGHT     0x7c
++#define PCKEY_DOWN      0x7d
++#define PCKEY_0   0x1d
++#define PCKEY_1   0x12
++#define PCKEY_2   0x13
++#define PCKEY_3   0x14
++#define PCKEY_4   0x15
++#define PCKEY_5   0x17
++#define PCKEY_6   0x16
++#define PCKEY_7   0x1a
++#define PCKEY_8   0x1c
++#define PCKEY_9   0x19
++#define PCKEY_0   0x1d
++#define PCKEY_A   0x00
++#define PCKEY_B   0x0b
++#define PCKEY_C   0x08
++#define PCKEY_D   0x02
++#define PCKEY_E   0x0e
++#define PCKEY_F   0x03
++#define PCKEY_G   0x05
++#define PCKEY_H   0x04
++#define PCKEY_I   0x22
++#define PCKEY_J   0x26
++#define PCKEY_K   0x28
++#define PCKEY_L   0x25
++#define PCKEY_M   0x2e
++#define PCKEY_N   0x2d
++#define PCKEY_O   0x1f
++#define PCKEY_P   0x23
++#define PCKEY_Q   0x0c
++#define PCKEY_R   0x0f
++#define PCKEY_S   0x01
++#define PCKEY_T   0x11
++#define PCKEY_U   0x20
++#define PCKEY_V   0x09
++#define PCKEY_W   0x0d
++#define PCKEY_X   0x07
++#define PCKEY_Y   0x10
++#define PCKEY_Z   0x06
++#define PCKEY_F1  0x7a
++#define PCKEY_F2  0x78
++#define PCKEY_F3  0x63
++#define PCKEY_F4  0x76
++#define PCKEY_F5  0x60
++#define PCKEY_F6  0x61
++#define PCKEY_F7  0x62
++#define PCKEY_F8  0x64
++#define PCKEY_F9  0x65
++#define PCKEY_F10 0x6d
++#define PCKEY_F11 0x6e
++#define PCKEY_F12 0x6f
++#define PCKEY_OEM_COMMA  0x2b
++#define PCKEY_SUBTRACT   0x1b
++#define PCKEY_OEM_PERIOD 0x2f
++#define PCKEY_DIVIDE     0x2c
+ 
+-#define PCKEY_BACK 59
+-#define PCKEY_TAB 56
+-#define PCKEY_CLEAR 0xff
+-#define PCKEY_RETURN 44
+-#define PCKEY_SHIFT_L 64
+-#define PCKEY_CONTROL_L 0x43
+-#define PCKEY_MENU 0x42 //option
+-#define PCKEY_PAUSE 121
+-#define PCKEY_CAPITAL 65
+-#define PCKEY_ESCAPE 61
+-#define PCKEY_SPACE 57
+-#define PCKEY_PRIOR 124
+-#define PCKEY_NEXT 129
+-#define PCKEY_END 127
+-#define PCKEY_HOME 123
+-#define PCKEY_LEFT 0x83
+-#define PCKEY_UP 0x86
+-#define PCKEY_RIGHT 0x84
+-#define PCKEY_DOWN 0x85
+-#define PCKEY_SELECT 0xff
+-#define PCKEY_PRINT 0xff
+-#define PCKEY_EXECUTE 0xff
+-#define PCKEY_SNAPSHOT 113
+-#define PCKEY_INSERT 122
+-#define PCKEY_DELETE 125
+-#define PCKEY_HELP 0xff
+-#define PCKEY_0 37
+-#define PCKEY_1 26
+-#define PCKEY_2 27
+-#define PCKEY_3 28
+-#define PCKEY_4 29
+-#define PCKEY_5 31
+-#define PCKEY_6 30
+-#define PCKEY_7 34
+-#define PCKEY_8 36
+-#define PCKEY_9 33
+-#define PCKEY_A 8
+-#define PCKEY_B 19
+-#define PCKEY_C 16
+-#define PCKEY_D 10
+-#define PCKEY_E 22
+-#define PCKEY_F 11
+-#define PCKEY_G 13
+-#define PCKEY_H 12
+-#define PCKEY_I 42
+-#define PCKEY_J 46
+-#define PCKEY_K 48
+-#define PCKEY_L 45
+-#define PCKEY_M 54
+-#define PCKEY_N 53
+-#define PCKEY_O 39
+-#define PCKEY_P 43
+-#define PCKEY_Q 20
+-#define PCKEY_R 23
+-#define PCKEY_S 9
+-#define PCKEY_T 25
+-#define PCKEY_U 40
+-#define PCKEY_V 17
+-#define PCKEY_W 21
+-#define PCKEY_X 15
+-#define PCKEY_Y 24
+-#define PCKEY_Z 14
+-#define PCKEY_STARTKEY 0x3f //command
++#define PCKEY_LBUTTON    0xff
++#define PCKEY_RBUTTON    0xff
++#define PCKEY_MBUTTON    0xff
++#define PCKEY_CANCEL     0xff
++#define PCKEY_CLEAR      0xff
++#define PCKEY_PAUSE      0xff
++#define PCKEY_CAPITAL    0xff
++#define PCKEY_PRIOR      0xff
++#define PCKEY_NEXT       0xff
++#define PCKEY_END        0xff
++#define PCKEY_HOME       0xff
++#define PCKEY_SELECT     0xff
++#define PCKEY_PRINT      0xff
++#define PCKEY_EXECUTE    0xff
++#define PCKEY_SNAPSHOT   0xff
++#define PCKEY_INSERT     0xff
++#define PCKEY_DELETE     0xff
++#define PCKEY_HELP       0xff
+ #define PCKEY_CONTEXTKEY 0xff
+-#define PCKEY_NUMPAD0 90
+-#define PCKEY_NUMPAD1 91
+-#define PCKEY_NUMPAD2 92
+-#define PCKEY_NUMPAD3 93
+-#define PCKEY_NUMPAD4 94
+-#define PCKEY_NUMPAD5 95
+-#define PCKEY_NUMPAD6 96
+-#define PCKEY_NUMPAD7 97
+-#define PCKEY_NUMPAD8 99
+-#define PCKEY_NUMPAD9 100
+-#define PCKEY_MULTIPLY 75
+-#define PCKEY_ADD 77
+-#define PCKEY_SEPARATOR 0xff
+-#define PCKEY_SUBTRACT 86
+-#define PCKEY_DECIMAL 73
+-#define PCKEY_DIVIDE 83
+-#define PCKEY_F1 130
+-#define PCKEY_F2 128
+-#define PCKEY_F3 107
+-#define PCKEY_F4 126
+-#define PCKEY_F5 104
+-#define PCKEY_F6 105
+-#define PCKEY_F7 106
+-#define PCKEY_F8 108
+-#define PCKEY_F9 109
+-#define PCKEY_F10 117
+-#define PCKEY_F11 111
+-#define PCKEY_F12 119
++#define PCKEY_NUMPAD0    0xff
++#define PCKEY_NUMPAD1    0xff
++#define PCKEY_NUMPAD2    0xff
++#define PCKEY_NUMPAD3    0xff
++#define PCKEY_NUMPAD4    0xff
++#define PCKEY_NUMPAD5    0xff
++#define PCKEY_NUMPAD6    0xff
++#define PCKEY_NUMPAD7    0xff
++#define PCKEY_NUMPAD8    0xff
++#define PCKEY_NUMPAD9    0xff
++#define PCKEY_OEM_MINUS  0xff
++#define PCKEY_MULTIPLY   0xff
++#define PCKEY_ADD        0xff
++#define PCKEY_SEPARATOR  0xff
++#define PCKEY_DECIMAL    0xff
+ #define PCKEY_F13 0xff
+ #define PCKEY_F14 0xff
+ #define PCKEY_F15 0xff
+@@ -449,23 +453,20 @@
+ #define PCKEY_F22 0xff
+ #define PCKEY_F23 0xff
+ #define PCKEY_F24 0xff
+-#define PCKEY_NUMLOCK 79
+-#define PCKEY_OEM_SCROLL 115
+-#define PCKEY_OEM_1 0x31
+-#define PCKEY_OEM_PLUS 0x20
+-#define PCKEY_OEM_COMMA 51
+-#define PCKEY_OEM_MINUS 0x23
+-#define PCKEY_OEM_PERIOD 55
+-#define PCKEY_OEM_2 0x34
+-#define PCKEY_OEM_3 0x12
+-#define PCKEY_OEM_4 0x29
+-#define PCKEY_OEM_5 0x32
+-#define PCKEY_OEM_6 0x26
+-#define PCKEY_OEM_7 0x2f
++#define PCKEY_NUMLOCK 0xff
++#define PCKEY_OEM_SCROLL 0xff
++#define PCKEY_OEM_1 0xff
++#define PCKEY_OEM_PLUS 0xff
++#define PCKEY_OEM_2 0xff
++#define PCKEY_OEM_3 0xff
++#define PCKEY_OEM_4 0xff
++#define PCKEY_OEM_5 0xff
++#define PCKEY_OEM_6 0xff
++#define PCKEY_OEM_7 0xff
+ #define PCKEY_OEM_8 0xff
+ #define PCKEY_ICO_F17 0xff
+ #define PCKEY_ICO_F18 0xff
+-#define PCKEY_OEM102 58
++#define PCKEY_OEM102 0xff
+ #define PCKEY_ICO_HELP 0xff
+ #define PCKEY_ICO_00 0xff
+ #define PCKEY_ICO_CLEAR 0xff
